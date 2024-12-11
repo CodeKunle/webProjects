7a7a5d550e017fcaae458f8e52d721d09e0b2c2b
CodeKunle <kunle.salis@gmail.com>
2024-12-11 16:58:29 +0100

    13th Update

--- clonedTwitter/src/Widget.jsx
+++ clonedTwitter/src/Widget.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
+import React from 'react';
+import SearchIcon from '@mui/icons-material/Search';
+import {
+  TwitterTimelineEmbed,
+  TwitterShareButton,
+  TwitterTweetEmbed,
+} from "react-twitter-embed";
+
 
 function Widget() {
   return (
     <div className='widget'>
-        <h2>Widget</h2>
-      
+      <div className='widget-input'>
+        <SearchIcon className="widget-seachIcon"/>
+        <input placeholder='Search Twitter' type='text'/>
+      </div>
+      <div className="widgetContainer">
+        <h2>What's happening</h2>
+        <TwitterTweetEmbed tweetId={"1800282963051225268"}  />
+        <TwitterTweetEmbed tweetId2={"1848431364145877174"}/>
+        <TwitterTimelineEmbed
+          sourceType="profile"
+          screenName="javaScript Daily"
+          options={{ height: 400 }}
+        />
+      </div>
     </div>
   )
 }
